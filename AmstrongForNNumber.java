364bcdd65b36629f3f3a16a3712a6d2bae631178
aksha <aksha@AJ>
2021-01-23 10:14:50 +0530

    reverse string

--- AmstrongForNNumber.java
+++ AmstrongForNNumber.java
@@ -1,5 +1,8 @@
 import java.util.*;
 public class AmstrongForNNumber {
+	public static void main(String[] args) {
+		
+	
     Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a Number");
 		int n = sc.nextInt();
---
+++ palindrom.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class palindrom {
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		int rev = 0;
+		int rem;
+		while(a != 0) {
+			rem = a %10;
+			rev = (rev * 10) + rem;
+			a = a/10;
+		}
+        System.out.println(rev);
+        sc.close();
+	}
+
+}
