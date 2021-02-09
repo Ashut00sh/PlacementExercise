68442ac4c9ad7f8a2ec32d896b86d0fc5d5d5dd4
aksha <aksha@AJ>
2021-02-09 10:40:22 +0530

    lastdigitnotprint

---
+++ CppPrac/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+        // if (num1[i]+num2[i]+carry>=10)
+        // {
+        //     carry=1;
+        // }else
+        // {
+        //     carry=0;
+        // }
--- CppPrac/twentydigitsum.cpp
+++ CppPrac/twentydigitsum.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
 void sumbigint(string a,string b){
     //find max length among two numbers
     int min=a.length()>b.length()?b.length():a.length();
@@ -9,13 +10,13 @@ void sumbigint(string a,string b){
     int num2[max];
     for (int i = a.length()-1;i>=0; i--)
     {
-        num1[i]=static_cast<int>(a.at(i)='0');
+        num1[i]=static_cast<int>(a.at(i)-'0');
         cout<<num1[i];
     }
     cout<<endl;
         for (int i = a.length()-1;i>=0; i--)
     {
-        num2[i]=static_cast<int>(a.at(i)='0');
+        num2[i]=static_cast<int>(b.at(i)-'0');
         cout<<num2[i];
     }
     cout<<endl;
@@ -23,14 +24,15 @@ void sumbigint(string a,string b){
     int sum[max+1];
     for (int i = 0; i < max; i++)
     {
+        cout<<"num1= "<<num1[i]<<endl;
+        cout<<"num2= "<<num2[i]<<endl;
+        cout<<"carry= "<<carry<<endl;
+        cout<<"result= "<<((num1[i]+num2[i]+carry)%10)<<endl;
         sum[i]=(num1[i]+num2[i]+carry)%10;
-        if (num1[i]+num2[i]+carry>=10)
-        {
+        if ((num1[i]+num2[i]+carry)>=10)
             carry=1;
-        }else
-        {
+            else
             carry=0;
-        }
         sum[max]=carry;
     }
     for (int i = 0; i < max; i++)
@@ -41,7 +43,7 @@ void sumbigint(string a,string b){
 }
 int main(){
     string a,b;
-    int sum,c,d;
+   // int sum,c,d;
     cout<<"Enter Two numbers "<<endl;
     cin>>a>>b;
     sumbigint(a,b);
