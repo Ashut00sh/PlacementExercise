903341ec7b965935580dd323e0754b0294f201a9
aksha <aksha@AJ>
2021-02-23 13:48:57 +0530

    vowels in string

---
+++ JavaPrac/Vowelstr.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Vowelstr{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string ");
+        String str=sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||
+            str.charAt(i)=='u'){
+                System.out.print("vowels present in string = "+str.charAt(i));
+            }
+        }
+        System.out.println();
+        sc.close();
+    }
+
+}
