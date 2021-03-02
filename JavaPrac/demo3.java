8dac4f5b1dc1f701ede6498fe2ae39ffca8bbc3e
aksha <aksha@AJ>
2021-03-02 09:18:37 +0530

    printf

--- JavaPrac/demo3.java
+++ JavaPrac/demo3.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 
 public class demo3{
     public static void main(String[] args) {
@@ -7,10 +8,8 @@ public class demo3{
                 case 0:
                     break;
                     case 1:System.out.println("one");
-                    case 2:System.out.println("tw");
-                    case 3:System.out.println("three");
-                    
-
+                    case 2:System.out.println("two");
+                    case 3:System.out.printf(Locale.getDefault(),"my name",1);
                 // default:
                 //     break;
             }
