c82480119844ddb09b7cdffe921a5c36cc261fa6
aksha <aksha@AJ>
2021-03-07 17:25:16 +0530

    when we create of obj of extended class abstract cnstr get call

---
+++ JavaPrac/AbstractDemo.java
@@ -0,0 +1,18 @@
+abstract class Abc{
+    Abc(){
+        System.out.println("abstract class Abc cnstr get called");
+    }
+   public abstract void show();
+}
+public class AbstractDemo extends Abc{
+    public static void main(String[] args) {
+        AbstractDemo obj=new AbstractDemo();
+        //obj.show();
+    }
+
+    @Override
+    public void show() {
+        System.out.println("define show of abstract method");
+        
+    }
+}
